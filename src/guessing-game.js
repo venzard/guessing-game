f04d7044a6f0d0aeb5d3770ed02a86539d95344c
venzard <vasilij-ch@ya.ru>
2022-08-16 22:36:14 +0300

    feat: task completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.res = Math.round(((this.max - this.min) / 2) + this.min);
+        return this.res;
     }
 
     lower() {
-
+        this.max = this.res;
     }
 
     greater() {
-
+        this.min = this.res;
     }
 }
 
